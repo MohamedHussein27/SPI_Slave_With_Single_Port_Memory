3fa20db258c4a80b40783481c5f04edf318cefe3
Mohamed Hussein <Mohamed_Hussein2100924@outlook.com>
2024-08-04 11:59:13 +0300

    Update design codes

--- RTL_Code/RAM.v
+++ RTL_Code/RAM.v
@@ -1,34 +1,48 @@
-module RAM #(parameter MEM_DEPTH = 256 , parameter ADDR_SIZE = 8)(
-    input clk , rst_n , rx_valid,
+module RAM #(
+    parameter MEM_DEPTH = 256,
+    parameter ADDR_SIZE = 8
+)(
+    input clk,
+    input rst_n,
+    input rx_valid,
     input [9:0] din,
     output reg [7:0] dout,
     output reg tx_valid
 );
+
 // Creating the memory array
 reg [ADDR_SIZE-1:0] memory [MEM_DEPTH-1:0]; //address size is the same as memory width
 
-reg [7:0] addr_wr ; //write address for writing data in it
-reg [7:0] addr_re ; //read address for reading data from it
+reg [7:0] addr_wr; // Write address for writing data
+reg [7:0] addr_re; // Read address for reading data 
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        dout <= 0;
-        tx_valid <= 0;
-        addr_wr <= 0;
-        addr_re <= 0;
+
+always @(posedge clk) begin  
+    if (!rst_n) begin   //making the reset synchronous so the memory is synthesized as a block
+        dout <= 8'b0;
+        tx_valid <= 1'b0;
+        addr_wr <= 8'b0;
+        addr_re <= 8'b0;
     end else begin
-        if (rx_valid) begin //once rx_valid is high then the din bus is completed and hence we will check on the first two bits 
-            if({din[9],din[8]} == 2'b00) //write address 
-                addr_wr <= din[7:0] ;
-            else if ({din[9],din[8]} == 2'b01)//write data in the address specified earlier
-                memory[addr_wr] <= din[7:0] ;
-            else if ({din[9],din[8]} == 2'b10)//read address 
-                addr_re <= din[7:0] ;
-            else if ({din[9],din[8]} == 2'b11) //Read data in the address specified  earlier and then sends the data in tx_data and make the tx_valid high
-            begin
-                dout <= memory[addr_re];
-                tx_valid <= 1;
-            end
+        if (rx_valid) begin // Once rx_valid is high, then the din bus is completed
+            case (din[9:8])
+                2'b00: begin
+                    addr_wr <= din[7:0]; // Write address
+                    tx_valid <= 1'b0;
+                end
+                2'b01: begin
+                    memory[addr_wr] <= din[7:0]; // Write data in the address specified earlier
+                    tx_valid <= 1'b0;
+                end
+                2'b10: begin
+                    addr_re <= din[7:0]; // Read address
+                    tx_valid <= 1'b0;
+                end
+                2'b11: begin // Read data in the address specified earlier and then send the data in dout and make tx_valid high
+                    dout <= memory[addr_re];
+                    tx_valid <= 1'b1;
+                end
+            endcase
         end
     end
 end
--- RTL_Code/SPI_SLAVE.v
+++ RTL_Code/SPI_SLAVE.v
@@ -9,6 +9,7 @@ localparam IDLE = 3'b000,
            WRITE = 3'b010,
            READ_ADD = 3'b011,
            READ_DATA = 3'b100;
+
 reg [2:0] cs , ns ; ///current state and next state           
 reg ADD_DATA_checker ; //if it's set to high then we will go to READ_ADD state and we will go READ_DATA state otherwise
 reg [3:0] counter1 ; //a counter to fill the rx_data (general counter)
@@ -16,23 +17,17 @@ reg [2:0] counter2 ; //specific counter for the READ_DATA state
 reg [9:0] bus ; // an internal signal to be filled and then assigned to rx_data so the data can be sent in parallel
 
 //state memory 
-always @(posedge clk or negedge rst_n )
+always @(posedge clk)
 begin
-    if(~rst_n) begin
+    if(~rst_n) 
         cs <= IDLE;
-        counter1 <= 9; //as the first bit entered will be the MSB
-        counter2 <= 7; // as the first bit outted will be the MSB
-        ADD_DATA_checker <= 1; // as reading address first is the default
-        rx_valid <= 0; 
-        MISO  <= 0; // making the default output is zero
-    end
-    else begin
+    else 
         cs <= ns ;
-    end   
 end
 
 //next state logic
 always @(*) begin
+    ns = cs ;
     case(cs)
         IDLE : begin
             if(ss_n)
@@ -74,57 +69,67 @@ always @(*) begin
 end
 
 //output logic 
-always @(posedge clk) begin
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        counter1 <= 9; //as the first bit entered will be the MSB
+        counter2 <= 7; // as the first bit outted will be the MSB
+        ADD_DATA_checker <= 1; // as reading address first is the default
+        bus <= 0;
+        rx_data <= 0;
+        rx_valid <= 0; 
+        MISO  <= 0; // making the default output is zero
+    end
     //IDLE state
-    if(cs == IDLE) begin
-        rx_valid <= 0;
-        counter1 <= 9 ; //to start the same proccess in other states without resetting
-        counter2 <= 7 ; 
+    else begin
+        if(cs == IDLE) begin
+            rx_valid <= 0;
+            counter1 <= 9 ; //to start the same proccess in other states without resetting
+            counter2 <= 7 ; 
+        end
+        //WRITE state
+        else if(cs == WRITE) begin
+            if (counter1 >= 0)begin
+                bus[counter1] <= MOSI;
+                counter1 <= counter1 - 1;   //decrement the counter to fill the whole output rx_data
+            end
+            if(counter1 == 4'b1111) begin//(4'b1111) means that the counter has the value -1 (the rx_data is completed)
+                rx_valid = 1;
+                rx_data <= bus ; //sending the parallel data to the RAM
+            end
+        end
+        //READ_ADD state  (mostly the same as WRITE state)
+        else if (cs == READ_ADD) begin
+            if (counter1 >= 0)begin
+                bus[counter1] <= MOSI;
+                counter1 <= counter1 - 1;   //decrement the counter to fill the whole output rx_data
+            end
+            if(counter1 == 4'b1111) begin//(4'b1111) means that the counter has the value -1 (the rx_data is completed)
+                rx_valid <= 1;
+                rx_data <= bus ; //sending the parallel data to the RAM
+                ADD_DATA_checker <= 0; //(means that the read address is recieved) as when this state ends we will go to the READ_DATA state 
+            end
+        end
+        //READ_DATA state  
+        else if (cs == READ_DATA) begin
+            if (counter1 >= 0)begin
+                bus[counter1] <= MOSI;
+                counter1 <= counter1 - 1;   //decrement the counter to fill the whole output rx_data
+            end
+            if(counter1 == 4'b1111) begin//(4'b1111) means that the counter has the value -1 (the rx_data is completed)
+                rx_valid <= 1;
+                rx_data <= bus ; //sending the parallel data to the RAM
+                counter1 <= 9 ; //only and only in this case we will reset the counter as we won't go back to the IDLE state until the process ends
+            end
+            if(rx_valid  == 1) rx_valid <= 0; //only and only in this case we will reset the rx_valid as we won't go back to the IDLE state until the process ends
+            if(tx_valid==1 && counter2 >=0)begin
+                MISO <= tx_data[counter2] ; //counter-2 as it it's an 8 bit bus not 10 bit bus 
+                counter2 <= counter2 - 1 ;
+            end
+            if(counter2 == 3'b111)begin
+                ADD_DATA_checker <= 1; //(means that we should send another address for reading in the next time) so we will go to READ_ADD state
+            end
+        end
     end
-    //WRITE state
-    else if(cs == WRITE) begin
-        if (counter1 >= 0)begin
-            bus[counter1] <= MOSI;
-            counter1 <= counter1 - 1;   //decrement the counter to fill the whole output rx_data
-        end
-        if(counter1 == 4'b1111) begin//(4'b1111) means that the counter has the value -1 (the rx_data is completed)
-            rx_valid = 1;
-            rx_data <= bus ; //sending the parallel data to the RAM
-        end
-    end
-    //READ_ADD state  (mostly the same as WRITE state)
-    else if (cs == READ_ADD) begin
-        if (counter1 >= 0)begin
-            bus[counter1] <= MOSI;
-            counter1 <= counter1 - 1;   //decrement the counter to fill the whole output rx_data
-        end
-        if(counter1 == 4'b1111) begin//(4'b1111) means that the counter has the value -1 (the rx_data is completed)
-            rx_valid <= 1;
-            rx_data <= bus ; //sending the parallel data to the RAM
-            ADD_DATA_checker <= 0; //(means that the read address is recieved) as when this state ends we will go to the READ_DATA state 
-        end
-    end
-    //READ_DATA state  
-    else if (cs == READ_DATA) begin
-        if (counter1 >= 0)begin
-            bus[counter1] <= MOSI;
-            counter1 <= counter1 - 1;   //decrement the counter to fill the whole output rx_data
-        end
-        if(counter1 == 4'b1111) begin//(4'b1111) means that the counter has the value -1 (the rx_data is completed)
-            rx_valid <= 1;
-            rx_data <= bus ; //sending the parallel data to the RAM
-            counter1 <= 9 ; //only and only in this case we will reset the counter as we won't go back to the IDLE state until the process ends
-        end
-        if(rx_valid  == 1) rx_valid <= 0; //only and only in this case we will reset the rx_valid as we won't go back to the IDLE state until the process ends
-        if(tx_valid==1 && counter2 >=0)begin
-            MISO <= tx_data[counter2] ; //counter-2 as it it's an 8 bit bus not 10 bit bus 
-            counter2 <= counter2 - 1 ;
-        end
-        if(counter2 == 3'b111)begin
-            ADD_DATA_checker <= 1; //(means that we should send another address for reading in the next time) so we will go to READ_ADD state
-        end
-    end
-
 
 end
 endmodule
